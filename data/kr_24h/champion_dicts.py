7530dab3196d13dd89e86c406da0f774fb1da05a
Ben Schumacher <benschumacher0@gmail.com>
2023-12-11 16:48:26 +0900

    added class to use models more easily

---
+++ data/kr_24h/champion_dicts.py
@@ -0,0 +1,87 @@
+import json
+
+# get closest string
+import difflib
+
+# converts between champion names, ids and indices
+
+class ChampionConverter:
+    def __init__(self):
+        self.champion_names = []
+        self.champion_ids = []
+        self.champion_indices = {}
+        self.champion_names_to_ids = {}
+        self.champion_ids_to_names = {}
+        self.champion_names_to_indices = {}
+        self.champion_indices_to_names = {}
+        self.champion_ids_to_indices = {}
+        self.champion_indices_to_ids = {}
+        self.load_champion_data()
+
+
+    def load_champion_data(self):
+        with open("champion.json", "r", encoding="utf-8") as f:
+            champion_data = json.load(f)
+
+        # insert null champion
+        self.champion_names.append("null")
+        self.champion_ids.append(0)
+
+        for champion in champion_data["data"].values():
+            self.champion_names.append(champion["id"])
+            self.champion_ids.append(int(champion["key"]))
+        names_and_ids = zip(self.champion_names, self.champion_ids)
+        names_and_ids = sorted(names_and_ids, key=lambda x: x[1])
+        self.champion_names, self.champion_ids = zip(*names_and_ids)
+        for i, champion_name in enumerate(self.champion_names):
+            self.champion_indices[champion_name] = i
+            self.champion_names_to_ids[champion_name] = self.champion_ids[i]
+            self.champion_names_to_indices[champion_name] = i
+            self.champion_indices_to_names[i] = champion_name
+        for i, champion_id in enumerate(self.champion_ids):
+            self.champion_ids_to_names[champion_id] = self.champion_names[i]
+            self.champion_ids_to_indices[champion_id] = i
+            self.champion_indices_to_ids[i] = champion_id
+
+        self.champion_count = len(self.champion_names)
+
+
+    def get_champion_name_from_index(self, champion_index):
+        return self.champion_indices_to_names[champion_index]
+
+    def get_champion_id_from_index(self, champion_index):
+        return self.champion_indices_to_ids[champion_index]
+
+    def get_champion_index_from_id(self, champion_id):
+        return self.champion_ids_to_indices[champion_id]
+
+    def get_champion_index_from_name(self, champion_name):
+        return self.champion_names_to_indices[champion_name]
+    
+    def get_champion_id_from_name(self, champion_name):
+        return self.champion_names_to_ids[champion_name]
+
+    def get_champion_name_from_id(self, champion_id):
+        return self.champion_ids_to_names[champion_id]
+    
+    def get_closest_champion_name(self, champion_name):
+        matches = difflib.get_close_matches(champion_name, self.champion_names, 1)
+        if len(matches) == 0:
+            return None
+        return matches[0]
+    
+
+if __name__ == "__main__":
+    conv = ChampionConverter()
+    while True:
+        champion_name = input("Enter champion name: ")
+        champ = conv.get_closest_champion_name(champion_name)
+        if champ is None:
+            print("No champion found!")
+            continue
+        print(champion_name, "->", champ)
+        # id
+        print("ID:", conv.get_champion_id_from_name(champ))
+        # index
+        print("Index:", conv.get_champion_index_from_name(champ))
+        print()
---
+++ lol_prediction.py
@@ -0,0 +1,150 @@
+import numpy as np
+import champion_dicts
+# this class is used with a model to perform different predictions based on game data
+
+class LoLPredictor:
+    def __init__(self, model):
+        self.model = model
+        self.converter = champion_dicts.ChampionConverter()
+
+    def convert_to_input(self, blue_team, red_team, format="names"):
+        """
+        Converts the inputs if this functions to inputs for the model.
+
+        Format = names | ids | idx
+        """
+
+        if len(blue_team) > 5 or len(red_team) > 5:
+            raise Exception("teams can't have more than 5 members")
+
+        if format == "names":
+            blue_team = self.converter.get_champion_index_from_name(blue_team)
+            red_team = self.converter.get_champion_index_from_name(red_team)
+
+        elif format == "ids":
+            blue_team = self.converter.get_champion_index_from_id(blue_team)
+            red_team = self.converter.get_champion_index_from_id(red_team)
+        
+        elif format == "idx":
+            # all good
+            pass
+        else:
+            raise Exception("Invalid Argument. Format has to be 'names', 'ids', or 'idx'")
+
+        # pad with zeroes
+        while len(blue_team)<5:
+            blue_team.append(0)
+        while len(red_team)<5:
+            red_team.append(0)
+
+        champs = blue_team + red_team
+        champs = np.array(champs)
+
+        return champs
+
+
+    def win_chance(self, blue_team, red_team, format="names") -> float:
+        """
+        Format = names | ids | idx
+        """
+
+        champs = self.convert_to_input(blue_team=blue_team, red_team=red_team, format=format)
+
+        champs = np.reshape(champs, (1, -1))
+
+        #print(champs)
+
+        wr = self.model.predict(champs)
+        wr = np.reshape(wr, (-1,))
+        #print(wr)
+        wr = float(wr)
+        return wr
+    
+    def best_pick(self, blue_team, red_team, available = [], format="names", print_details=True) -> str:
+        """
+        Find the champion with the best win chance for the next pick.
+        Tries only champions in available, unless len(available)==0.
+
+
+        Format = names | ids | idx
+        """
+
+        if len(red_team)>=5 and red_team[4]!=0:
+            raise Exception("Red team can't have 5 members to predict the next champion.")
+
+        if len(available)>0:
+            if format == "names":
+                available = self.converter.get_champion_index_from_name(available)
+
+            elif format == "ids":
+                available = self.converter.get_champion_index_from_id(available)
+        else:
+            available = self.converter.get_all_indices()
+
+        
+
+        champs = self.convert_to_input(blue_team=blue_team, red_team=red_team, format=format)
+
+        # remove champions that were already picked
+        available = list(filter(lambda i: i not in champs, available))
+        available = np.array(available)
+
+
+        # find which position should be replaced
+        order_to_pos = [0, 5, 6, 1, 2, 7, 8, 3, 4, 9]
+        pos = -1
+        for i in range(10):
+            idx = order_to_pos[i]
+            if champs[idx] == 0:
+                pos = idx
+                break
+
+        if pos == -1:
+            raise Exception("Failed to find empty position...")
+        
+        user_blue = pos < 5
+
+        
+
+        matches = np.tile(champs, len(available))
+        matches = matches.reshape((-1, 10))
+        matches[:, pos] = available
+
+        results = self.model.predict(matches)
+
+        # sorts all_champs based on results
+        results = results.reshape((-1,))
+        if not user_blue:
+            # since the model only predict wr for blue side, wr is 1-res if user is on red side
+            results = 1 - results
+        indicies = np.argsort(results)
+
+
+        indicies = indicies[::-1]
+
+        available = available[indicies]
+        results = results[indicies]
+
+        combined = np.column_stack((available, results))
+
+        if print_details:
+            blue = [ self.converter.get_champion_name_from_index(i) for i in champs[:5]]
+            red = [ self.converter.get_champion_name_from_index(i) for i in champs[5:]]
+
+            print("5 best picks for")
+            print(blue)
+            print("vs")
+            print(red)
+            for i in range(min(len(available), 5)):
+                wr = results[i]
+                wr_rounded = round(float(wr) * 100, 2)
+                champ = available[i]
+                print(champ)
+                champ_name = self.converter.get_champion_name_from_index(int(champ))
+                print(f"{champ_name}: {wr_rounded}%")
+
+        return combined
+
+        
+
+        
