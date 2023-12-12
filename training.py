d9cef5196b9db3d59657134f222bc4ceab17ec39
Otso Weckström <otso@Otsos-MacBook-Pro.local>
2023-12-12 11:35:42 +0900

    menu and ensemble

--- training.py
+++ training.py
@@ -9,8 +9,9 @@ import matplotlib.pyplot as plt
 CHAMP_NUM = 170
 PLAYER_NUM = 10
 
+
 # load data
-DATAPATH = "data/kr_24h/game_data_filtered.npy"
+DATAPATH = "data/kr_24h/game_data.npy"
 data = np.load(DATAPATH)
 print("Loaded data of shape: ", data.shape)
 
@@ -72,7 +73,7 @@ train_x, _, val_x, _, test_x, _ = split_data(x, y)
 
 
 avg_win_chance = np.average(train_y)
-print("Average Blue side win chance: ", avg_win_chance)
+
 
 class TrivialModel(tf.keras.Model):
     """A trivial model that always predicts the average win chance"""
@@ -112,7 +113,6 @@ class BaselineModel(tf.keras.Model):
 
 
 def plot_hist(hist) -> None:
-    
     plt.plot(hist.history['accuracy'])
     plt.plot(hist.history['val_accuracy'])
     plt.title('model accuracy')
@@ -171,6 +171,9 @@ from models.lol_transformer import LoLTransformer
 from models.deep_embedding import DeepEmbedding
 
 from models.prob_sample_model import SamplingModel
+
+from models.ensemble_model import EnsembleModel
+
 import stats
 
 model0 = TrivialModel()
@@ -180,63 +183,57 @@ model0.compile(optimizer=tf.keras.optimizers.Adam(),
 
 hist0 = model0.fit(train_x, train_y, epochs=3, validation_data=(val_x, val_y), batch_size=64, callbacks=[scheduler])
 
-
 #model = DeepConv(emb_dim=32, conv_layers=3)
-model = BasicEmbedding()
-model.compile(optimizer=tf.keras.optimizers.Adam(),
+model1 = BasicEmbedding()
+model1.compile(optimizer=tf.keras.optimizers.Adam(),
               loss=tf.keras.losses.BinaryCrossentropy(),
               metrics=['accuracy'])
 
-hist = model.fit(aug, epochs=3, validation_data=(val_aug_x, val_aug_y), batch_size=64, callbacks=[scheduler])
+hist1 = model1.fit(aug, epochs=3, validation_data=(val_aug_x, val_aug_y), batch_size=64, callbacks=[scheduler])
 
-model2 = DeepEmbedding(n_layers=2)
+model2 = DeepEmbedding(n_layers=2) 
 model2.compile(optimizer=tf.keras.optimizers.Adam(),
                 loss=tf.keras.losses.BinaryCrossentropy(),
                 metrics=['accuracy'])
 
 hist2 = model2.fit(aug, epochs=3, validation_data=(val_aug_x, val_aug_y), batch_size=64, callbacks=[scheduler])
 
+MODEL = model1
+HIST = hist1
+
+
+# Create an ensemble model
+#model3 = EnsembleModel(models=[model0, model1, model2])
+#model3.compile(optimizer=tf.keras.optimizers.Adam(),
+                       #loss=tf.keras.losses.BinaryCrossentropy(),
+                      # metrics=['accuracy'])
+
+
+#hist3 = model3.fit(train_x, train_y, epochs=3, validation_data=(val_x, val_y), batch_size=64, callbacks=[scheduler])
+
+
+
+
 comp = stats.ModelComparator((test_x, test_y))
 comp.add_model(model0, hist0, "TrivialModel")
-comp.add_model(model, hist, "BasicEmbedding")
+comp.add_model(model1, hist1, "BasicEmbedding")
 comp.add_model(model2, hist2, "DeepEmbedding")
+#comp.add_model(model3, hist3, "EnsembleModel")
 
-comp.plot_histories()
-comp.plot_bar()
-comp.print_summary()
-comp.print_table()
 
 from lol_prediction import LoLPredictor
 
-pred = LoLPredictor(model)
 
-chance = pred.win_chance(["Ahri", "Elise", "Singed"], ["DrMundo", "Kassadin"])
-print(chance)
-
-pred.best_pick(["Ahri", "Elise", "Singed"], ["DrMundo", "Kassadin"])
-
-ret = pred.best_pick(["Ahri", "Elise", "Singed"], ["DrMundo", "Draven"], available=["Taliyah", "Yone", "Orianna"])
-
-print(ret)
 
 # fit without augmentation
 #hist = model.fit(aug, epochs=3, validation_data=(val_x, val_y), batch_size=32, callbacks=[scheduler])
 #hist = model.fit(train_x_1h, train_y, epochs=5, validation_data=(val_x_1h, val_y), batch_size=32, callbacks=[scheduler])
 
-stats.print_embedding_norms(model.embedding, CHAMP_NUM)
-
-stats.visualize_embeddings(model.embedding, CHAMP_NUM)
-
-# test accuracy
-test_loss, test_acc = model.evaluate(test_x, test_y, verbose=2)
-print("Test accuracy: ", test_acc)
-print("Test loss: ", test_loss)
-
-plot_hist(hist)
 # save model weights
 #model.save_weights("models/lol_transformer_8_8.h5")
 
 def find_optimal_champion():
+    print("finding optimal champions")
     # get random sample from test data
     l = test_x.shape[0]
     idx = np.random.randint(0, l)
@@ -262,7 +259,7 @@ def find_optimal_champion():
     matches = matches.reshape((-1, 10))
     matches[:, remove_num] = all_champs[:]
 
-    results = model.predict(matches)
+    results = MODEL.predict(matches)
 
     # sorts all_champs based on results
     results = results.reshape((-1,))
@@ -285,9 +282,141 @@ def find_optimal_champion():
         champ_name = conv.get_champion_name_from_index(int(champ))
         print(f"{champ_name}: {wr_rounded}%")
 
-while True:
+
+# Choose model
+# Champion prediction
+# Display blue sides average win rate
+# Display comp plots
+# Plot history
+# Visualize embeddings
+# Print test accuracy and loss
+
+def menu():
+    print("")
+    print("1. Choose Model")
+    print("2. Find Optimal Champion")
+    print("3. Display Blue Sides Average Win Rate")
+    print("4. Display Comp Plots")
+    print("5. Plot History")
+    print("6. Visualize Embeddings")
+    print("7. Print Test Accuracy and Loss")
+    print("8. Win Chance of Two Champions Head to Head")
+    print("9. LoLPredictor")
+    print("0. Exit")
+
+def option1():
+    global MODEL
+    global HIST
+    print("Choose the model used for the predictions")
+    print("1. Trivial Model")
+    print("2. BasicEmbedding")
+    print("3. DeepEmbedding")
+    model_choice = input("Enter your choice (1-3): ")
+    print("")
+
+    if model_choice == "1":
+        MODEL = model0
+        HIST = hist0
+        print("Model set to Trivial Model")
+    elif model_choice == "2":
+        MODEL = model1
+        HIST = hist1
+        print("Model set to Basic Embedding")
+    elif model_choice == "3":
+        MODEL = model2
+        HIST = hist2
+        print("Model set to Deep Embedding")
+    #elif model_choice == "4":
+     #   MODEL = model3
+      #  HIST = hist3
+       # print("Model set to Ensemble Model")
+    else:
+        print("Invalid choice. Please enter a number between 1 and 3.")
+        print("")
+
+def option2():
     find_optimal_champion()
-    input()
+
+def option3():
+    print("Average Blue side win chance: ", avg_win_chance)
+   
+
+def option4():
+    comp.plot_histories()
+    comp.plot_bar()
+    comp.print_summary()
+    comp.print_table()
+
+def option5():
+    plot_hist(HIST)
+
+def option6():
+    stats.print_embedding_norms(MODEL.embedding, CHAMP_NUM)
+    stats.visualize_embeddings(MODEL.embedding, CHAMP_NUM)
+    
+
+def option7():
+    test_loss, test_acc = MODEL.evaluate(test_x, test_y, verbose=2)
+    print("Test accuracy: ", test_acc)
+    print("Test loss: ", test_loss)
+
+def option8():
+    champ1 = input("Champ 1: ")
+    champ2 = input("Champ 2: ")
+    closest1 = conv.get_closest_champion_name(champ1)
+    closest2 = conv.get_closest_champion_name(champ2)
+    idx1 = conv.champion_names_to_indices[closest1]
+    idx2 = conv.champion_names_to_indices[closest2]
+    test_in = [idx1]*5 + [idx2]*5
+    test_in = np.array(test_in)
+
+    y_pred = MODEL.predict(test_in.reshape(1,10))
+
+    print("Win chance of ", closest1, " vs ", closest2, ": ", y_pred[0][0])
+
+def option9():
+    pred = LoLPredictor(MODEL)
+
+    chance = pred.win_chance(["Ahri", "Elise", "Singed"], ["DrMundo", "Kassadin"])
+    print(chance)
+
+    pred.best_pick(["Ahri", "Elise", "Singed"], ["DrMundo", "Kassadin"])
+
+    ret = pred.best_pick(["Ahri", "Elise", "Singed"], ["DrMundo", "Draven"], available=["Taliyah", "Yone", "Orianna"])
+
+    print(ret)
+
+while True:
+    menu()
+    choice = input("Enter your choice (0-9): ")
+    print("")
+    if choice == "1":
+        option1()
+    elif choice == "2":
+        option2()
+    elif choice == "3":
+        option3()
+    elif choice == "4":
+        option4()
+    elif choice == "5":
+        option5()
+    elif choice == "6":
+        option6()
+    elif choice == "7":
+        option7()
+    elif choice == "8":
+        option8()
+    elif choice == "9":
+        option9()
+    elif choice == "0":
+        print("Exiting the program. Goodbye!")
+        break
+    else:
+        print("Invalid choice. Please enter a number between 0 and 9.")
+
+
+
+
 
 
 
@@ -304,7 +433,7 @@ while True:
     test_in = [idx1]*5 + [idx2]*5
     test_in = np.array(test_in)
 
-    y_pred = model.predict(test_in.reshape(1,10))
+    y_pred = MODEL.predict(test_in.reshape(1,10))
 
     print("Win chance of ", closest1, " vs ", closest2, ": ", y_pred[0][0])
 
